e7ca0686b6fe58b36d2eaabc97f23519e47f5810
prodhanm <mustafizur.prodhan@gmail.com>
2024-12-12 15:16:12 -0500

    built and tested the method and it works

---
+++ CreditCardStr.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CreditCardStr {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter a credit card number: ");
+        String ccNumber = inp.nextLine();
+        System.out.println("Credit card number: " + ccSingleString(ccNumber));
+        inp.close();
+    }
+
+    public static String ccSingleString(String ccNum) {
+        return ccNum.replace("-", "");
+    }
+    
+}
